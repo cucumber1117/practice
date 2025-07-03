82c22fdc4c8ac11e95e4d70fd409c32d5d112a7b
kiiroiiruka <127270769+kiiroiiruka@users.noreply.github.com>
2025-07-03 15:13:02 +0900

    あ

--- src/components/slideDown/slideDown.jsx
+++ src/components/slideDown/slideDown.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import styles from './slideDown.module.css'
 
 const SlideDown = ({ children }) => {
-  // 開閉状態を内部で管理
+  // 開閉状態を内部で管理(@fix7:Headerの開いているか開いていないかの値の状態もHeade内で管理しておくと良いかも)
   const [show, setShow] = useState(false)
 
   const toggleShow = () => setShow(prev => !prev)
--- src/pages/Level7/Level7.jsx
+++ src/pages/Level7/Level7.jsx
@@ -4,7 +4,7 @@ import Footer from '@/components/Footer/Footer'
 import List from '@/components/List/List'
 import styles from './Level7.module.css'
 import SlideDown from '@/components/SlideDown/SlideDown'
-import exampleImage from '@/assets/images/Camera.png'
+import exampleImage from '@/assets/images/Camera.png'//@fix7:基本的には画像はassetsフォルダ内にimagesフォルダを作成してその中に保存することが多いい。
 
 const Level7 = () => {
     const navigate = useNavigate()
@@ -28,6 +28,7 @@ const Level7 = () => {
                 <button className={styles.Button} onClick={goToLevel8}>
                     レベル8へ
                 </button>
+                {/*@fix7:で画像を表示*/}
                 <img src={exampleImage} alt="説明テキスト" className={styles.roundImage} />
             </div>
             <Footer title="レベル7" />
